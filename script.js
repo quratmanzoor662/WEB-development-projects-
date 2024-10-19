fbbfff329d1cc029a3ecb4e3cc1721665de04df8
Qurat Manzoor <161446609+quratmanzoor662@users.noreply.github.com>
2024-10-19 12:32:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// script.js
+function appendToDisplay(value) {
+    const display = document.getElementById('display');
+    display.value += value;
+  }
+  
+  function clearDisplay() {
+    document.getElementById('display').value = '';
+  }
+  
+  function calculateResult() {
+    const display = document.getElementById('display');
+    try {
+      display.value = eval(display.value);
+    } catch (e) {
+      display.value = 'Error';
+    }
+  }  
